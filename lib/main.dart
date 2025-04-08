cc210293566f92abbd88e491a48319f7ff853970
eizw <hamzahamri2005@gmail.com>
2025-04-08 16:17:59 +0800

    Clean

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'screens/MainScreenPage.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
